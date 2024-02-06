0572f41eea6ae950bac8b900e069422b5e7e284f
Alex Girard <alex.girard.2@ulaval.ca>
2024-02-05 20:53:55 -0500

    Add dropdown searchbar on Homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <h1>Home</h1>
-    <div>GLO-3102 Home</div>
-    
-  </div>
-  
+    <div class="input-group mb-3">
+  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+  <input type="text" class="form-control" aria-label="Text input with dropdown button">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+</div>
+    </div>
 </template>
 
 <style>
