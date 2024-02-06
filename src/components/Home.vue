b93e39efd5f8521013a7f7204014717c17d5219f
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-02-05 21:58:00 -0500

    feat: add cards to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,20 +2,57 @@
   <div>
     <h1>Home</h1>
     <div class="input-group mb-3">
-  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">All</button>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><a class="dropdown-item" href="#">Nearby</a></li>
+    <li><a class="dropdown-item" href="#">Cheap</a></li>
+    <li><a class="dropdown-item" href="#">Expensive</a></li>
     <li><hr class="dropdown-divider"></li>
     <li><a class="dropdown-item" href="#">Separated link</a></li>
   </ul>
   <input type="text" class="form-control" aria-label="Text input with dropdown button">
   <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-</div>
+    </div>
+    <div class="container-lg">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img src="https://picsum.photos/200" class="card-img-top" style="width: 10em" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant name</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant name</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant name</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant name</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 </template>
-
 <style>
 
 </style>
+<script setup>
+</script>
