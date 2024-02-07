6c9370218f046d3e13318e04cc4a010714590bf8
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-02-07 01:03:56 -0500

    feat: add layout homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,87 @@
 <template>
   <div>
     <h1>Catchphrase here + logo</h1>
-    <div class="input-group mb-3">
-      <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">All</button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Nearby</a></li>
-        <li><a class="dropdown-item" href="#">Cheap</a></li>
-        <li><a class="dropdown-item" href="#">Expensive</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Separated link</a></li>
-      </ul>
-      <input type="text" class="form-control" aria-label="Text input with dropdown button">
-      <button class="btn btn-primary" type="button" id="button-addon1">Button</button>
-    </div>
     <div class="container">
       <div class="row">
         <div class="col-3">
-          <div class="card rounded-5 bg-c-blue">
-            <img src="https://picsum.photos/200" class="card-img-top rounded-top-5" alt="...">
-            <h3 class="card-title">Restaurant</h3>
+          <div class="input-group">
+            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+            <button class="btn btn-primary" type="button" id="button-addon1">Button</button>
+          </div>
+          <ul class="list-group mt-3">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
+          </ul>
+          <label for="customRange1" class="form-label">Price range</label>
+          <input type="range" class="form-range bv-no-focus-ring" id="customRange1">
+          <ul class="list-group mt-3">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
+          </ul>
+        </div>
+
+        <div class="col-6 d-flex flex-wrap row-gap-3 justify-content-evenly align-items-center">
+          <div class="card rounded-4 border-0 shadow" style="width: 15em">
+            <img src="https://picsum.photos/200" class="card-img-top p-3 rounded-5" alt="...">
             <div class="card-body">
+              <h3 class="card-title">Restaurant</h3>
             </div>
           </div>
+          <div class="card rounded-4 border-0 shadow" style="width: 15em">
+            <img src="https://picsum.photos/200" class="card-img-top p-3 rounded-5" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant</h3>
+            </div>
+          </div>
+          <div class="card rounded-4 border-0 shadow" style="width: 15em">
+            <img src="https://picsum.photos/200" class="card-img-top p-3 rounded-5" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant</h3>
+            </div>
+          </div>
+          <div class="card rounded-4 border-0 shadow" style="width: 15em">
+            <img src="https://picsum.photos/200" class="card-img-top p-3 rounded-5" alt="...">
+            <div class="card-body">
+              <h3 class="card-title">Restaurant</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+          <h2 class="text-center">Friends</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th class="text-end" scope="col">pts</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>William</td>
+                <td class="text-end">2350</td>
+              </tr>
+              <tr>
+                <td>Cédric</td>
+                <td class="text-end">1990</td>
+              </tr>
+              <tr>
+                <td>Alex</td>
+                <td class="text-end">1800</td>
+              </tr>
+              <tr>
+                <td>Edwin</td>
+                <td class="text-end">1180</td>
+              </tr>
+            </tbody>
+          </table>
+          <button class="btn btn-primary" type="button">Add friends</button>
         </div>
       </div>
     </div>
