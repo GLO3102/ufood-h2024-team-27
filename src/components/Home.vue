89076e5f1d0058b0540f3753d1d2a65e532d566d
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-02-06 23:05:06 -0500

    feat: modify sass colors bootstrap

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,25 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Catchphrase here + logo</h1>
     <div class="input-group mb-3">
-  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">All</button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Nearby</a></li>
-    <li><a class="dropdown-item" href="#">Cheap</a></li>
-    <li><a class="dropdown-item" href="#">Expensive</a></li>
-    <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#">Separated link</a></li>
-  </ul>
-  <input type="text" class="form-control" aria-label="Text input with dropdown button">
-  <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+      <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">All</button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Nearby</a></li>
+        <li><a class="dropdown-item" href="#">Cheap</a></li>
+        <li><a class="dropdown-item" href="#">Expensive</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Separated link</a></li>
+      </ul>
+      <input type="text" class="form-control" aria-label="Text input with dropdown button">
+      <button class="btn btn-primary" type="button" id="button-addon1">Button</button>
     </div>
-    <div class="container-lg">
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card">
-            <img src="https://picsum.photos/200" class="card-img-top" style="width: 10em" alt="...">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <div class="card rounded-5 bg-c-blue">
+            <img src="https://picsum.photos/200" class="card-img-top rounded-top-5" alt="...">
+            <h3 class="card-title">Restaurant</h3>
             <div class="card-body">
-              <h3 class="card-title">Restaurant name</h3>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h3 class="card-title">Restaurant name</h3>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h3 class="card-title">Restaurant name</h3>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h3 class="card-title">Restaurant name</h3>
             </div>
           </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "../src/assets/scss/custom.css";
 
 const app = createApp(App);
 
