40fe39f19f4467d04adf4c524be7e839823b091c
Will Guilbert <williamguilbert@hotmail.com>
2024-02-08 19:02:45 -0500

    User page
    Added user page design, Still need to implement .json info and modify the styles a bit.

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,8 +1,71 @@
 <template>
-  <div>
-    <h1>User Profile</h1>
-    <div>GLO-3102 User Profile page</div>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-10 mt-3 pt-3 pb-5">
+        <div class="row z-depth-3 shadow-lg mb-1 mt-3  border border-5 rounded border-primary">
+          <div class="col-sm-4 bg-primary rounded-left">
+            <div class="card-block text-center text-white">
+              <img
+                src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"
+                class="rounded-circle mb-3 mt-5"
+                style="width: 150px"
+                alt="Avatar"
+              />
+
+              <h5 class="mb-2">
+                <strong>User 1 <span class="badge bg-danger">PRO</span></strong>
+              </h5>
+              <p class="text-muted" id="bio">Typical eater</p>
+              <i class="fa-solid fa-star w-100 pb-4" style="color: #ffd43b"
+                >1</i
+              >
+              <i class="far fa-edit fa-2x mb-4"></i>
+            </div>
+          </div>
+          <div class="col-sm-8 bg-white">
+            <h3 class="mt-3 text-center">Information</h3>
+            <hr class="border border-secondary border-1 opacity-50" />
+
+            <div class="row">
+              <div class="col-6 mt-3 ml-2">
+                <p class="fw-bold">
+                  First Name
+                </p>
+                <h6 class="fw-normal text-muted">User</h6>
+              </div>
+              <div class="col-6 mt-3">
+                <p class="fw-bold">
+                  Last Name
+                </p>
+                <h6 class="fw-normal text-muted">1</h6>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-6 mt-3">
+                <p class="fw-bold">Email</p>
+                <h6 class="fw-normal text-muted">User1@ulaval.ca</h6>
+              </div>
+              <div class="col-6 mt-3">
+                <p class="fw-bold">Phone</p>
+                <h6 class="fw-normal text-muted">418-999-1234</h6>
+              </div>
+              <div class="col-12 mt-3">
+                <p class="fw-bold">City</p>
+                <h6 class="fw-normal text-muted">Quebec</h6>
+                <hr class="border border-secondary border-1 opacity-50" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.container{
+   
+}
+
+</style>
