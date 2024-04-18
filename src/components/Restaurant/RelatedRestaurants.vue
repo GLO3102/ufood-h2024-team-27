8b7a7436cf8c09add648ec8f35fa4d45c3073314
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-04-18 01:47:31 -0400

    misc: lint code

--- src/components/Restaurant/RelatedRestaurants.vue
+++ src/components/Restaurant/RelatedRestaurants.vue
@@ -1,18 +1,28 @@
 <template>
-
-
   <div class="container">
     <div class="card rounded-5 p-3 py-2">
       <div class="card-body">
         <h2 class="card-title">Related Restaurants</h2>
       </div>
-      <div class="col-md-4" v-for="restaurant in relatedRestaurants" :key="restaurant.id">
+      <div
+        class="col-md-4"
+        v-for="restaurant in relatedRestaurants"
+        :key="restaurant.id"
+      >
         <div class="card rounded-5 p-3 py-2">
-          <img :src="restaurant.pictures[0]" class="card-img-top" alt="Image du restaurant">
+          <img
+            :src="restaurant.pictures[0]"
+            class="card-img-top"
+            alt="Image du restaurant"
+          />
           <div class="card-body">
             <h5 class="card-title">{{ restaurant.name }}</h5>
             <p class="card-text">{{ restaurant.description }}</p>
-            <router-link :to="`/restaurant/${restaurant.id}`" class="btn btn-primary">Voir plus</router-link>
+            <router-link
+              :to="`/restaurant/${restaurant.id}`"
+              class="btn btn-primary"
+              >Voir plus</router-link
+            >
             <!-- Pour l'instant in faut faire un refresh manuel -->
           </div>
         </div>
@@ -30,7 +40,7 @@ export default {
   data() {
     return {
       relatedRestaurants: [],
-      relatedLoaded: false
+      relatedLoaded: false,
     };
   },
   async created() {
@@ -40,20 +50,27 @@ export default {
     async fetchRelatedRestaurants() {
       const params = { genres: this.genre, limit: 6 };
       try {
-        const response = await apiGetRestaurants(params, this.$cookies.get('user_cookie'));
+        const response = await apiGetRestaurants(
+          params,
+          this.$cookies.get("user_cookie"),
+        );
         this.relatedRestaurants = response.items;
         this.filterRestaurants();
         this.relatedLoaded = true;
       } catch (error) {
-        console.error("Erreur lors de la récupération des restaurants similaires : ", error);
+        console.error(
+          "Erreur lors de la récupération des restaurants similaires : ",
+          error,
+        );
       }
     },
     filterRestaurants() {
-      this.relatedRestaurants = this.relatedRestaurants.filter(restaurant => restaurant.id !== this.currentRestaurantId);
-    }
-  }
+      this.relatedRestaurants = this.relatedRestaurants.filter(
+        (restaurant) => restaurant.id !== this.currentRestaurantId,
+      );
+    },
+  },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
